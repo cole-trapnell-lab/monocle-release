a1776915a128ce062993db44fd0a7e436191968e
Cole Trapnell <cole@broadinstitute.org>
2014-07-10 12:36:32 -0700

    removed old website, moved monocle packages comments to top level of repo, removed HSMMSingleCell, which now has its own repo

--- HSMMSingleCell/R/HSMMSingleCell.R
+++
@@ -1,9 +0,0 @@
-#' @name HSMM
-#' @title Single-cell expression data for HSMM cells
-#' @description This dataset contains expression profiles from a time-series study of differentiating human skeletal muscle myoblasts (HSMM). Expression values are in FPKM units, as calculated with the Cufflinks package.
-#' @docType data
-#' @usage HSMM
-#' @format a \code{CellDataSet} object, as provided by the \code{monocle} package.
-#' @source monocle
-#' @author Cole Trapnell, 03-17-2014
-NULL
