e8453785f911613d26bf4e15c6782d948fd00c27
Xiaojie Qiu <xqiu@uw.edu>
2015-12-05 19:25:03 -0800

    debug the DDRTree algorithm and add documentation

--- R/DDRTree.R
+++ R/DDRTree.R
@@ -1,10 +1,11 @@
-#pca_projection function: 
-# X : DxN data matrix
-# params.
-#       maxIter : maximum iterations
-#       eps     : relative objective difference
-#       dim     : reduced dimension
-#       lambda  : regularization parameter for inverse graph embedding
+#' perform PCA projection
+#' solve the problem size(C) = NxN, size(W) = NxL
+#' max_W trace( W' C W ) : W' W = I
+#' @param C a matrix with N x N dimension
+#' @param L a dataframe used to generate new data for interpolation of time points
+#' @return a matrix (W) with N x L dimension
+#' @export
+#' 
 pca_projection <- function(C, L) {
 	eigen_res <- eigen(C)
 
@@ -16,16 +17,22 @@ pca_projection <- function(C, L) {
 	W <- U[, eig_idx[1:L]]
 }
 
-# a, b : [D, N] = size(a)
-# calculate the square distance between a, b
+#' perform PCA projection
+#' solve the problem size(C) = NxN, size(W) = NxL
+#' max_W trace( W' C W ) : W' W = I
+#' @param a a matrix with D x N dimension
+#' @param b a matrix with D x N dimension
+#' @return a numeric value for the different between a and b
+#' @export
+#' 
 sqdist <- function(a, b) {
-	aa <- rowSums(a^2)
-	bb <- rowSums(b^2)
-	ab <- t(a) * b
+	aa <- colSums(a^2)
+	bb <- colSums(b^2)
+	ab <- t(a) %*% b
 
-	aa_repmat <- matrix(rep(aa, times = ncol(bb)), ncol = ncol(bb), byrow = T)
-	bb_repmat <- matrix(rep(bb, times = ncol(aa)), ncol = ncol(aa), byrow = F)
-	dist <- abs(aa_repmat + bb_repmat - 2 * a * b)
+	aa_repmat <- matrix(rep(aa, times = ncol(b)), ncol = ncol(b), byrow = F)
+	bb_repmat <- matrix(rep(bb, times = ncol(a)), nrow = ncol(a), byrow = T)
+	dist <- abs(aa_repmat + bb_repmat - 2 * ab)
 }
 
 # X : DxN data matrix
@@ -36,16 +43,29 @@ sqdist <- function(a, b) {
 #       lambda  : regularization parameter for inverse graph embedding
 #       sigma   : bandwidth parameter
 #       gamma   : regularization parameter for k-means
-DDRTree <- function(X, params) {
+#' Perform DDRTree construction
+#' @param X a matrix with D x N dimension which is needed to perform DDRTree construction
+#' @param params a list with the following parameters: 
+#' maxIter : maximum iterations
+#' eps     : relative objective difference
+#' dim     : reduced dimension
+#' lambda  : regularization parameter for inverse graph embedding
+#' sigma   : bandwidth parameter
+#' gamma   : regularization parameter for k-means
+#' @return a list with W, Z, stree, Y, history
+#' @export
+#' gamma   : regularization parameter for k-means
+#' 
+DDRTree <- function(X, params, verbose = F) {
 
 	D <- nrow(X) 
 	N <- ncol(X)
 
 	#initialization
-	W <- pca_projection(X * t(X), params$dim)
-	Z <- t(W) * W
+	W <- pca_projection(X %*% t(X), params$dim)
+	Z <- t(W) %*% X
 
-	if('ncenter' %in% names(params)) {
+	if(!('ncenter' %in% names(params))) {
 		K <- N 
 		Y <- Z[, 1:K]
 	}
@@ -58,88 +78,75 @@ DDRTree <- function(X, params) {
 
 	#main loop: 
 	objs <- c()
+	history <- list()
 	for(iter in 1:params$maxIter) {
 
-		#Kruskal method to find optimal B (use RBGL algorithm: http://stackoverflow.com/questions/16605825/minimum-spaning-tree-with-kruskal-algorithm)
+# 		#Kruskal method to find optimal B (use RBGL algorithm: http://stackoverflow.com/questions/16605825/minimum-spaning-tree-with-kruskal-algorithm)
 		distsqMU <- sqdist(Y, Y)
-		#convert with graph packagege to BAM class of graph an calculate mst
-		mstKruskalBAM <- mstree.kruskal(graphBAM(Y))
-		#build new data frame with resut
-		stree <- data.frame(cbind(t(mstKruskalBAM$edgeList),
-		                                 t(mstKruskalBAM$weight)))
-		stree <- stree + t(stree)
+# 		#convert with graph packagege to BAM class of graph an calculate mst
+# 		mstKruskalBAM <- mstree.kruskal(graphBAM(as.data.frame(distsqMU)))
+# 		#build new data frame with resut
+# 		stree <- data.frame(cbind(t(mstKruskalBAM$edgeList),
+# 		                                 t(mstKruskalBAM$weight)))
+
+	  ##########################use mst from igraph: ##########################
+	  g <- graph.adjacency(distsqMU, mode = 'lower', diag = T, weighted = T)
+	  g_mst <- mst(g)		
+	  stree <- get.adjacency(g_mst, attr = 'weight', type = 'lower')
+	  
+	  #convert to matrix: 
+	  stree <- as.matrix(stree)
+	  stree <- stree + t(stree)
 		B_tmp <- stree != 0
-		B_tmp[B_tmp == 0] <- 0
-		B_tmp[B_tmp == 1] <- 1
+		B <- B_tmp
+		B[B_tmp == FALSE] <- 0
+		B[B_tmp == TRUE] <- 1
 		L <- diag(colSums(B)) - B
 
 		# #convert back to igraph package
 		# stree <- graph.data.frame(mstKruskalDF, directed=FALSE)
-
+		
 		#compute R usingmean-shift update rule 
 		distZY <- sqdist(Z, Y)
-		min_dist <- matrix(rep(apply(distZY, 2, min), times = K), ncol = K, byrow = T)
+		min_dist <- matrix(rep(apply(distZY, 1, min), times = K), ncol = K, byrow = F)
 		tmp_distZY <- distZY - min_dist
-		tmp_R <- exp(-tmp_distZY ./ params$sigma)
-		R <- tmp_R ./ matrix(rep(rowSums(tmp_R), times = K), byrow = T)
-		Gamma <- diag(sum(R))
+		tmp_R <- exp(-tmp_distZY / params$sigma)
+		R <- tmp_R / matrix(rep(rowSums(tmp_R), times = K), byrow = F, ncol = K)
+		Gamma <- matrix(rep(0, ncol(R) ^ 2), nrow = ncol(R))
+		diag(Gamma) <- colSums(R)
 
 		#termination condition 
-		obj1 <- - params$sigma * sum(log(rowSums(exp(-tmp_distZY ./ params$sig))) 
-				- min_dist[, 1] ./params$sigma)
-		objs[iter] <- (norm(X - W * Z))^2 + params$lambda .* trace(Y * L * t(Y)) + params$gamma * obj1
+		obj1 <- - params$sigma * sum(log(rowSums(exp(-tmp_distZY / params$sigma))) 
+				- min_dist[, 1] /params$sigma)
+		objs[iter] <- (norm(X - W %*% Z, '2'))^2 + params$lambda * sum(diag(Y %*% L %*% t(Y))) + params$gamma * obj1 #sum(diag(A))
 
 		if(verbose)
-			message('iter = ', obj, ' = ', iter, ' ', objs[iter])
+			message('iter = ', iter, ' ', objs[iter])
 
-		histor$W[iter] <- W
-		histor$Z[iter] <- Z
-		histor$Y[iter] <- Y
-		histor$stree[iter] <- stree
-		histor$R[iter] <- R
+		history$W[iter] <- W
+		history$Z[iter] <- Z
+		history$Y[iter] <- Y
+		history$stree[iter] <- stree
+		history$R[iter] <- R
 
 		if(iter > 1) {
-			if(objs[iter] - objs[iter - 1] / abs(objs[iter - 1])) < params$eps {
+			if(abs(objs[iter] - objs[iter - 1]) / abs(objs[iter - 1]) < params$eps) {
 				break
 			}
 
 		}
 
 		#compute low dimension projection matrix
-		tmp <- R / (((params$gamma + 1) / params$gamma) .* ((params$lambda / params$gamma) .* L + gamma) - t(R) * R)
-		Q <- 1 / (params$gamma + 1) .* (eye(N, N) + tmp * t(R))
-		C <- X * Q
-		tmp1 <- C * t(X)
-		W <- pca_projection((tmp1 + t(tmp1)) ./ 2, params$dim)
-		Z <- t(W) * C
-		Y <- Z * R / (params$lambda / params$gamma .* L + Gamma)
+		tmp <- t(solve((((params$gamma + 1) / params$gamma) * ((params$lambda / params$gamma) * L + Gamma) - t(R) %*% R), t(R))) 
+		Q <- 1 / (params$gamma + 1) * (diag(1, N) + tmp %*% t(R))
+		C <- X %*% Q
+		tmp1 <- C %*% t(X)
+		W <- pca_projection((tmp1 + t(tmp1)) / 2, params$dim)
+		Z <- t(W) %*% C
+		Y <- t(solve((params$lambda / params$gamma * L + Gamma), t(Z %*% R)))
 	}
 
 	history$objs <- objs
+	  
+	return(list(W = W, Z = Z, stree = stree, Y = Y, history = history))
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
---
+++ test.DDRTree.R
@@ -0,0 +1,79 @@
+library(monocle)
+context("genSmoothCurves")
+
+test_that("DDRTRee() perform the DDRTree construction", {
+  
+ko_exprs <- readMat('/Users/xqiu/Dropbox (Personal)/bifurcation_path/simplePPT/data/ko_exprs.mat')
+ko_exprs <- as.matrix(ko_exprs$ko.exprs)
+storage.mode(ko_exprs) <- 'numeric'
+X <- ko_exprs
+X <- log2(X + 1)
+X <- apply(X, 1, function(x) x - mean(x))
+X <- t(X)
+
+pca_res <- pca_projection(X[, ] %*% t(X [, ]), 2)
+sqdist_res <- sqdist(X, X)
+params <- list(maxIter = 20, eps = 1e-3, dim = 2, lambda = 2435, sigma = 1e-3, gamma = 10)
+DDRTree_res <- DDRTree(X = X, params = params, T)
+qplot(x = DDRTree_res$Y[1, ], y = DDRTree_res$Y[2, ])
+
+#ko tree: 
+ko_DDRTree_res <- DDRTree_res
+
+#lung data:
+lung_exprs <- readMat('/Users/xqiu/Dropbox (Personal)/bifurcation_path/simplePPT/data/lung_exprs_mat.mat')
+lung_exprs <- as.matrix(lung_exprs$x)
+storage.mode(lung_exprs) <- 'numeric'
+X <- lung_exprs
+X <- log2(X + 1)
+X <- apply(X, 1, function(x) x - mean(x))
+X <- t(X)
+
+pca_res <- pca_projection(X[, ] %*% t(X [, ]), 2)
+sqdist_res <- sqdist(X, X)
+params <- list(maxIter = 20, eps = 1e-3, dim = 2, lambda = 2435, sigma = 1e-3, gamma = 10)
+DDRTree_res <- DDRTree(X = X, params = params, T)
+qplot(x = DDRTree_res$Y[1, ], y = DDRTree_res$Y[2, ])
+
+#lung tree: 
+lung_DDRTree_res <- DDRTree_res
+
+#lung data:
+golgi_exprs <- readMat('/Users/xqiu/Dropbox (Personal)/bifurcation_path/simplePPT/data/golgi_exprs.mat')
+golgi_exprs <- as.matrix(golgi_exprs$golgi.exprs)
+storage.mode(golgi_exprs) <- 'numeric'
+X <- golgi_exprs
+X <- log2(X + 1)
+X <- apply(X, 1, function(x) x - mean(x))
+X <- t(X)
+
+pca_res <- pca_projection(X[, ] %*% t(X [, ]), 2)
+sqdist_res <- sqdist(X, X)
+params <- list(maxIter = 20, eps = 1e-3, dim = 2, lambda = 2435, sigma = 1e-3, gamma = 10)
+DDRTree_res <- DDRTree(X = X, params = params, T)
+qplot(x = DDRTree_res$Y[1, ], y = DDRTree_res$Y[2, ])
+
+#lung tree: 
+golgi_DDRTree_res <- DDRTree_res
+
+#HSMM data:
+valid_subset_GSE72857_exprs <- readMat('/Users/xqiu/Dropbox (Personal)/bifurcation_path/simplePPT/data/valid_subset_GSE72857.mat')
+valid_subset_GSE72857_exprs <- as.matrix(valid_subset_GSE72857_exprs$valid.subset.GSE72857)
+storage.mode(valid_subset_GSE72857_exprs) <- 'numeric'
+X <- valid_subset_GSE72857_exprs
+X <- log2(X + 1)
+X <- apply(X, 1, function(x) x - mean(x))
+X <- t(X)
+
+pca_res <- pca_projection(X[, ] %*% t(X [, ]), 2)
+sqdist_res <- sqdist(X, X)
+params <- list(maxIter = 20, eps = 1e-3, dim = 2, lambda = 5 * ncol(X), sigma = 1e-3, gamma = 10)
+DDRTree_res <- DDRTree(X = X, params = params, T)
+qplot(x = DDRTree_res$Y[1, ], y = DDRTree_res$Y[2, ])
+
+#lung tree: 
+valid_subset_GSE72857_DDRTree_res <- DDRTree_res
+qplot(x = DDRTree_res$Y[1, ], y = DDRTree_res$Y[2, ], color = MAP_cells_exprs)
+
+
+})
