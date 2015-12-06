8497d738ce3e91dba9838f439e25740a6af0fcba
Xiaojie Qiu <xqiu@uw.edu>
2015-12-05 20:01:13 -0800

    update the test.DDRTRee testing

--- tests/testthat/test.DDRTree.R
+++ tests/testthat/test.DDRTree.R
@@ -1,8 +1,14 @@
 library(monocle)
-context("genSmoothCurves")
+library('R.matlab')
+library(igraph)
+library(ggplot2)
+context("DDRTRee")
+
+#note that dim (the inherent dimension the data reduced to and where the tree is constructed) can be changed (currently it is 2)
 
 test_that("DDRTRee() perform the DDRTree construction", {
   
+#ko data:
 ko_exprs <- readMat('/Users/xqiu/Dropbox (Personal)/bifurcation_path/simplePPT/data/ko_exprs.mat')
 ko_exprs <- as.matrix(ko_exprs$ko.exprs)
 storage.mode(ko_exprs) <- 'numeric'
@@ -38,7 +44,7 @@ qplot(x = DDRTree_res$Y[1, ], y = DDRTree_res$Y[2, ])
 #lung tree: 
 lung_DDRTree_res <- DDRTree_res
 
-#lung data:
+#golgi data:
 golgi_exprs <- readMat('/Users/xqiu/Dropbox (Personal)/bifurcation_path/simplePPT/data/golgi_exprs.mat')
 golgi_exprs <- as.matrix(golgi_exprs$golgi.exprs)
 storage.mode(golgi_exprs) <- 'numeric'
@@ -53,10 +59,10 @@ params <- list(maxIter = 20, eps = 1e-3, dim = 2, lambda = 2435, sigma = 1e-3, g
 DDRTree_res <- DDRTree(X = X, params = params, T)
 qplot(x = DDRTree_res$Y[1, ], y = DDRTree_res$Y[2, ])
 
-#lung tree: 
+#golgi tree: 
 golgi_DDRTree_res <- DDRTree_res
 
-#HSMM data:
+#Cell data:
 valid_subset_GSE72857_exprs <- readMat('/Users/xqiu/Dropbox (Personal)/bifurcation_path/simplePPT/data/valid_subset_GSE72857.mat')
 valid_subset_GSE72857_exprs <- as.matrix(valid_subset_GSE72857_exprs$valid.subset.GSE72857)
 storage.mode(valid_subset_GSE72857_exprs) <- 'numeric'
@@ -71,9 +77,29 @@ params <- list(maxIter = 20, eps = 1e-3, dim = 2, lambda = 5 * ncol(X), sigma = 
 DDRTree_res <- DDRTree(X = X, params = params, T)
 qplot(x = DDRTree_res$Y[1, ], y = DDRTree_res$Y[2, ])
 
-#lung tree: 
+#cell tree: 
 valid_subset_GSE72857_DDRTree_res <- DDRTree_res
 qplot(x = DDRTree_res$Y[1, ], y = DDRTree_res$Y[2, ], color = MAP_cells_exprs)
 
+#cell tree: 
+cell_DDRTree_res <- DDRTree_res
+
+#HSMM data:
+HSMM_exprs <- readMat('/Users/xqiu/Dropbox (Personal)/bifurcation_path/simplePPT/data/HSMM_exprs.mat')
+HSMM_exprs <- as.matrix(HSMM_exprs$HSMM.exprs)
+storage.mode(HSMM_exprs) <- 'numeric'
+X <- HSMM_exprs
+X <- log2(X + 1)
+X <- apply(X, 1, function(x) x - mean(x))
+X <- t(X)
+
+pca_res <- pca_projection(X[, ] %*% t(X [, ]), 2)
+sqdist_res <- sqdist(X, X)
+params <- list(maxIter = 20, eps = 1e-3, dim = 2, lambda = 2435, sigma = 1e-3, gamma = 10)
+DDRTree_res <- DDRTree(X = X, params = params, T)
+qplot(x = DDRTree_res$Y[1, ], y = DDRTree_res$Y[2, ])
+
+#HSMM tree: 
+HSMM_DDRTree_res <- DDRTree_res
 
 })
