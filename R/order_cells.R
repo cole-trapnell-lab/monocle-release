879cb714a96d7586cb0d5a92af7ab8d70ee18c69
Xiaojie <xqiu@uw.edu>
2017-04-12 11:17:04 -0700

    fix getting pc / variance by irlba

--- R/order_cells.R
+++ R/order_cells.R
@@ -1397,21 +1397,25 @@ reduceDimension <- function(cds,
       else{
         num_dim <- 50
       }
-
-      irlba_res <- irlba(FM,
-                         nv=min(num_dim, min(dim(FM)) - 1),
-                             nu=0,
-                             center=cell_means,
-                             scale=sqrt(cell_vars),
-                             right_only=TRUE)
-      irlba_pca_res <- irlba_res$v
-      #row.names(irlba_pca_res) <- genes_to_keep
+      
+      irlba_res <- prcomp_irlba(t(FM), n = min(num_dim, min(dim(FM)) - 1),
+                                center = TRUE, scale = TRUE)
+      irlba_pca_res <- irlba_res$x
+      
+      # irlba_res <- irlba(FM,
+      #                    nv=min(num_dim, min(dim(FM)) - 1),
+      #                        nu=0,
+      #                        center=cell_means,
+      #                        scale=sqrt(cell_vars),
+      #                        right_only=TRUE)
+      # irlba_pca_res <- irlba_res$v
+      # row.names(irlba_pca_res) <- genes_to_keep
 
       # pca_res <- prcomp(t(FM), center = T, scale = T)
       # std_dev <- pca_res$sdev
       # pr_var <- std_dev^2
       # prop_varex <- pr_var/sum(pr_var)
-      prop_varex <- irlba_res$d / sum(irlba_res$d)
+      prop_varex <- irlba_res$sdev^2 / sum(irlba_res$sdev^2)
 
       topDim_pca <- irlba_pca_res#[, 1:num_dim]
 
--- R/plotting.R
+++ R/plotting.R
@@ -1870,17 +1870,21 @@ plot_pc_variance_explained <- function(cds,
     
     # FM <- convert2DRData(cds, norm_method = 'log') 
     # FM <- FM[rowSums(is.na(FM)) == 0, ]
-    cell_means <- Matrix::rowMeans(FM_t)
-    cell_vars <- Matrix::rowMeans((FM_t - cell_means)^2)
-    
-    irlba_res <- irlba(FM,
-                       nv= min(max_components, min(dim(FM)) - 1), #avoid calculating components in the tail
-                       nu=0,
-                       center=cell_means,
-                       scale=sqrt(cell_vars),
-                       right_only=TRUE)
-    prop_varex <- irlba_res$d / sum(irlba_res$d)
-    
+    irlba_res <- prcomp_irlba(t(FM), n = min(max_components, min(dim(FM)) - 1),
+                              center = TRUE, scale = TRUE)
+    prop_varex <- irlba_res$sdev^2 / sum(irlba_res$sdev^2)
+    # 
+    # cell_means <- Matrix::rowMeans(FM_t)
+    # cell_vars <- Matrix::rowMeans((FM_t - cell_means)^2)
+    # 
+    # irlba_res <- irlba(FM,
+    #                    nv= min(max_components, min(dim(FM)) - 1), #avoid calculating components in the tail
+    #                    nu=0,
+    #                    center=cell_means,
+    #                    scale=sqrt(cell_vars),
+    #                    right_only=TRUE)
+    # prop_varex <- irlba_res$d / sum(irlba_res$d)
+    # 
     # # pca_res <- prcomp(t(FM), center = T, scale = T)
     # # std_dev <- pca_res$sdev 
     # # pr_var <- std_dev^2
