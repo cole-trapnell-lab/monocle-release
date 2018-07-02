1e791486bfbcd7489abd9f44667c04aa7cb69e29
Andysheh Mohajeri <andyshehmohajeri@gs103956.gs.washington.edu>
2018-07-02 14:07:07 -0700

    prepped files for rebase

--- R/clustering.R
+++ R/clustering.R
@@ -45,11 +45,14 @@ clusterGenes<-function(expr_matrix, k, method=function(x){as.dist((1 - cor(Matri
 #' is big, for example > 50 k, we recommend the user to use the Louvain clustering 
 #' algorithm which is inspired from phenograph paper. Note Louvain doesn't support the 
 #' num_cluster argument but the k (number of k-nearest neighbors) is relevant to the final 
-#' clustering number. The implementation of Louvain clustering is based on the Rphenograph
-#' package but updated based on our requirement (for example, changed the jaccard_coeff 
-#' function as well as adding louvain_iter argument, etc.)  
+#' clustering number. The deafult implementation of Louvain clustering (when res is set to be NULL)
+#' is based on the Rphenograph package but updated based on our requirement (for example,
+#' changed the jaccard_coeff function as well as adding louvain_iter argument, etc.). We also 
+#' support setting resolutioin parameter when performing louvain clustering using the louvain package 
+#' from python. With different res values, the users can obtain different granularity of the data.  
 #' 
 #' @param cds the CellDataSet upon which to perform this operation
+#' @param use_pca Whether or not to cluster cells based on top PCA component. Default to be FALSE. 
 #' @param skip_rho_sigma A logic flag to determine whether or not you want to skip the calculation of rho / sigma 
 #' @param num_clusters Number of clusters. The algorithm use 0.5 of the rho as the threshold of rho and the delta 
 #' corresponding to the number_clusters sample with the highest delta as the density peaks and for assigning clusters
@@ -82,6 +85,7 @@ clusterGenes<-function(expr_matrix, k, method=function(x){as.dist((1 - cor(Matri
 #' @export
 
 clusterCells <- function(cds, 
+                         use_pca = FALSE,
                          skip_rho_sigma = F, 
                          num_clusters = NULL, 
                          inspect_rho_sigma = F, 
@@ -109,7 +113,11 @@ clusterCells <- function(cds,
   if(method == 'densityPeak'){ 
     ################### DENSITYPEAK START ###################
     set.seed(2017)
-    tsne_data <- reducedDimA(cds)
+    if(use_pca) {
+      tsne_data <- t(cds@normalized_data_projection)
+    } else {
+      tsne_data <- reducedDimA(cds)
+    }
     if(nrow(tsne_data) == 0) {
       message('ReduceDimension is not applied to this dataset. We are using the normalized reduced space obtained from preprocessCDS to cluster cells...')
       tsne_data <- t(cds@normalized_data_projection)
@@ -208,19 +216,23 @@ clusterCells <- function(cds,
     
     ################### DENSITYPEAK END ###################
   }  else if(method == 'louvain'){
-    data <- t(reducedDimA(cds))
+    if(use_pca) {
+      data <- cds@normalized_data_projection
+    } else {
+      data <- t(reducedDimA(cds))
+    }
     if(nrow(data) == 0) {
       message('ReduceDimension is not applied to this dataset. We are using the normalized reduced space obtained from preprocessCDS to cluster cells...')
       data <- cds@normalized_data_projection
-    }
-    
-    if(!('louvain_res' %in% names(cds@auxOrderingData[[cds@dim_reduce_type]]))) {
-      louvain_res <- louvain_clustering(data, pData(cds), k, weight, louvain_iter, res, verbose)
+      louvain_res <- louvain_clustering(data, pData(cds), k, weight, louvain_iter, res, verbose, ...)
     } else {
-      louvain_res <- cds@auxOrderingData[[cds@dim_reduce_type]]$louvain_res     
+      if(!('louvain_res' %in% names(cds@auxOrderingData[[cds@dim_reduce_type]]))) {
+        louvain_res <- louvain_clustering(data, pData(cds), k, weight, louvain_iter, res, verbose, ...)
+      } else {
+        louvain_res <- cds@auxOrderingData[[cds@dim_reduce_type]]$louvain_res     
+      }
     }
-    
-    cluster_graph_res <- compute_louvain_connected_components(louvain_res$g, louvain_res$optim_res, verbose = verbose, ...)
+    cluster_graph_res <- compute_louvain_connected_components(louvain_res$g, louvain_res$optim_res, verbose = verbose)
     louvain_component <-  components(cluster_graph_res$cluster_g)$membership[louvain_res$optim_res$membership]
     names(louvain_component) <- V(louvain_res$g)$name
     louvain_component <- as.factor(louvain_component)
@@ -237,54 +249,6 @@ clusterCells <- function(cds,
   }
 }
 
-#' #' Select features for constructing the developmental trajectory 
-#' #' 
-#' #' @param cds the CellDataSet upon which to perform this operation
-#' #' @param num_cells_expressed A logic flag to determine whether or not you want to skip the calculation of rho / sigma 
-#' #' @param num_dim Number of clusters. The algorithm use 0.5 of the rho as the threshold of rho and the delta 
-#' #' corresponding to the number_clusters sample with the highest delta as the density peaks and for assigning clusters
-#' #' @param rho_threshold The threshold of local density (rho) used to select the density peaks 
-#' #' @param delta_threshold The threshold of local distance (delta) used to select the density peaks 
-#' #' @param qval_threshold A logic flag passed to densityClust function in desnityClust package to determine whether or not Gaussian kernel will be used for calculating the local density
-#' #' @param verbose Verbose parameter for DDRTree
-#' #' @return an list contains a updated CellDataSet object after clustering and tree construction as well as a vector including the selected top significant differentially expressed genes across clusters of cells  
-#' #' @references Rodriguez, A., & Laio, A. (2014). Clustering by fast search and find of density peaks. Science, 344(6191), 1492-1496. doi:10.1126/science.1242072
-#' #' @export
-#' dpFeature <- function(cds, num_cells_expressed = 5, num_dim = 5, rho_threshold = NULL, delta_threshold = NULL, qval_threshold = 0.01, verbose = F){
-#'   #1. determine how many pca dimension you want:
-#'   cds <- detectGenes(cds)
-#'   fData(cds)$use_for_ordering[fData(cds)$num_cells_expressed > num_cells_expressed] <- T
-#'   
-#'   if(is.null(num_dim)){
-#'     lung_pc_variance <- plot_pc_variance_explained(cds, return_all = T)
-#'     ratio_to_first_diff <- diff(lung_pc_variance$variance_explained) / diff(lung_pc_variance$variance_explained)[1]
-#'     num_dim <- which(ratio_to_first_diff < 0.1) + 1
-#'   }
-#'   
-#'   #2. run reduceDimension with tSNE as the reduction_method
-#'   # absolute_cds <- setOrderingFilter(absolute_cds, quake_id)
-#'   cds <- reduceDimension(cds, return_all = F, max_components=2, norm_method = 'log', reduction_method = 'tSNE', num_dim = num_dim,  verbose = verbose)
-#'   
-#'   #3. initial run of clusterCells_Density_Peak
-#'   cds <- clusterCells_Density_Peak(cds, rho_threshold = rho_threshold, delta_threshold = delta_threshold, verbose = verbose)
-#'   
-#'   #perform DEG test across clusters: 
-#'   cds@expressionFamily <- negbinomial.size()
-#'   pData(cds)$Cluster <- factor(pData(cds)$Cluster)
-#'   clustering_DEG_genes <- differentialGeneTest(cds, fullModelFormulaStr = '~Cluster', cores = detectCores() - 2)
-#'   clustering_DEG_genes_subset <- lung_clustering_DEG_genes[fData(cds)$num_cells_expressed > num_cells_expressed, ]
-#'   
-#'   #use all DEG gene from the clusters
-#'   clustering_DEG_genes_subset <- clustering_DEG_genes_subset[order(clustering_DEG_genes_subset$qval), ]
-#'   ordering_genes <- row.names(subset(clustering_DEG_genes, qval < qval_threshold))
-#'   
-#'   cds <- setOrderingFilter(cds, ordering_genes = lung_ordering_genes)
-#'   cds <- reduceDimension(cds, norm_method = 'log', verbose = T)
-#'   plot_cell_trajectory(cds, color_by = 'Time')
-#'   
-#'   return(list(new_cds = cds, ordering_genes = ordering_genes))
-#' }
-
 #' function to run louvain clustering algorithm 
 #'
 #' @param data low dimensional space used to perform graph clustering 
@@ -292,11 +256,9 @@ clusterCells <- function(cds,
 #' @param k number of nearest neighbors used for Louvain clustering 
 #' @param weight whether or not to calculate the weight for each edge in the kNN graph 
 #' @param louvain_iter the number of iteraction for louvain clustering 
-#' @param resolution resolution of clustering result
+#' @param resolution resolution of clustering result, specifiying the granularity of clusters
 #' @param verbose Whether to emit verbose output during dimensionality reduction
 #' @param ... extra arguments used to run louvain_R
-#' @importFrom utils txtProgressBar setTxtProgressBar
-#' @import pbapply
 #' @return a list with four elements (g (igraph object for the kNN graph), coord (coordinates of the graph with 
 #' layout_component, if the number of cells is less than 3000), edge_links (the data frame to plot the edges of 
 #' the igraph, if the number of cells is less than 3000) and optim_res (the louvain clustering result)). 
@@ -350,7 +312,6 @@ louvain_clustering <- function(data, pd, k = 20, weight = F, louvain_iter = 1, r
   Qp <- -1
   optim_res <- NULL
   best_max_resolution <- 'No resolution'
-  pb1 <- txtProgressBar(min = 0, max = length(louvain_iter), style = 3, file = stderr())
   for (iter in 1:louvain_iter) {
     if (verbose) {
       cat("Running louvain iteration ", iter, "...\n")
@@ -371,7 +332,6 @@ louvain_clustering <- function(data, pd, k = 20, weight = F, louvain_iter = 1, r
           Qp <- Qt
           best_max_resolution <- cur_resolution
         }
-        setTxtProgressBar(pb = pb1, value = pb1$getVal() + 1)
       }
     } else {
       Q <- igraph::cluster_louvain(g)
@@ -435,7 +395,6 @@ compute_louvain_connected_components <- function(g, optim_res, qval_thresh=0.05,
   edges_per_module = rowSums(num_links)
   total_edges = sum(num_links)
   
-  pb2 <- txtProgressBar(min = 0, max = length(louvain_modules), style = 3, file = stderr())
   for(i in 1:length(louvain_modules)){
     for(j in 1:length(louvain_modules)){
       
@@ -452,7 +411,6 @@ compute_louvain_connected_components <- function(g, optim_res, qval_thresh=0.05,
       cluster_mat[i,j] = p_val_i_j
       enrichment_mat[i,j] = num_links_i_j
     }
-    setTxtProgressBar(pb = pb2, value = pb2$getVal() + 1)
   }
   cluster_mat = matrix(p.adjust(cluster_mat), nrow=length(louvain_modules), ncol=length(louvain_modules))
   #cluster_mat[cluster_mat > qval_thresh] = 0
@@ -481,161 +439,4 @@ compute_louvain_connected_components <- function(g, optim_res, qval_thresh=0.05,
   edge_links$weight <- edge[, 3]
   
   list(cluster_g = cluster_g, cluster_optim_res = optim_res, cluster_coord = coord, edge_links = edge_links)
-}
-
-# Function to retrieve a graph of cell clusters 
-cluster_graph <- function(pc_g, g, optim_res, data, verbose = FALSE) {
-  # V(pc_g)$name <- as.character(as.integer(V(pc_g)))
-  # V(g)$name <- as.character(as.integer(V(g)))
-  
-  ########################################################################################################################################################################
-  # identify edges which pass through two clusters 
-  # remove edges pass through two clusters that has low overlapping 
-  ########################################################################################################################################################################
-  # pc_g <- minSpanningTree(cds)
-  # g <- cds@auxOrderingData[[cds@dim_reduce_type]]$louvain_res$g
-  # optim_res <- cds@auxOrderingData[[cds@dim_reduce_type]]$louvain_res$optim_res
-  cell_membership <- igraph::membership(optim_res)
-  cell_names <- names(cell_membership)
-  n_cluster <- length(unique(cell_membership))
-  
-  cluster_mat_exist <- matrix(0, nrow = n_cluster, ncol = n_cluster) # a matrix storing the overlapping clusters between louvain clusters which is based on the spanning tree
-  overlapping_threshold <- 1e-5
-  
-  # delete edge 75|2434from current cluster 8 and target cluster 18with weight 0
-  # delete edge 487|879from current cluster 8 and target cluster 18with weight 0
-  
-  pb3 <- utils::txtProgressBar(min = 0, max = length(sort(unique(as.vector(cell_membership)))), style = 3, file = stderr())
-  for(i in sort(unique(as.vector(cell_membership)))) {
-    if(verbose) {
-      message('current cluster is ', i)
-    }
-    curr_cluster_cell <- cell_names[which(cell_membership == i)]
-    
-    neigh_list <- igraph::neighborhood(pc_g, nodes = curr_cluster_cell)
-    # identify connected cells outside a Louvain group
-    conn_cluster_res <- do.call(rbind, pblapply(1:length(curr_cluster_cell), function(x) {
-      cell_outside <- setdiff(neigh_list[[x]]$name, curr_cluster_cell)
-      if(length(cell_outside)) {
-        data.frame(current_cell = curr_cluster_cell[x], 
-                   cell_outside = cell_outside,
-                   current_cluster =  cell_membership[curr_cluster_cell[x]],
-                   target_cluster = cell_membership[cell_outside])
-      }
-    } ))
-    
-    # remove the insignificant inter-cluster edges from the kNN graph: 
-    if(is.null(conn_cluster_res) == FALSE) {
-      for(j in 1:nrow(conn_cluster_res)) {
-        overlapping_clusters <- cell_names[which(cell_membership == conn_cluster_res[j, 'target_cluster'])]
-        all_ij <- igraph::ecount(igraph::subgraph(g, c(curr_cluster_cell, overlapping_clusters))) # edges in all cells from two Louvain landmark groups
-        only_i <- igraph::ecount(igraph::subgraph(g, curr_cluster_cell)) # edges from the first Louvain landmark groups
-        only_j <- igraph::ecount(igraph::subgraph(g, overlapping_clusters)) # edges from the second Louvain landmark groups
-        
-        overlap_weight <- (all_ij - only_i - only_j) / all_ij
-        cluster_mat_exist[conn_cluster_res[j, 'current_cluster'], conn_cluster_res[j, 'target_cluster']] <- overlap_weight
-        if(overlap_weight < overlapping_threshold) { # edges overlapping between landmark groups
-          # if(verbose) {
-          #   message('delete edge ', paste0(conn_cluster_res[j, 'current_cell'], "|", conn_cluster_res[j, 'cell_outside']), 
-          #           'from current cluster ', conn_cluster_res[j, 'current_cluster'], ' and target cluster ', conn_cluster_res[j, 'target_cluster'],
-          #           'with weight ', overlap_weight)
-          # }
-          pc_g <- pc_g %>% igraph::delete_edges(paste0(conn_cluster_res[j, 'current_cell'], "|", conn_cluster_res[j, 'cell_outside']))
-        }
-      }
-    }
-    utils::setTxtProgressBar(pb = pb3, value = pb3$getVal() + 1)
-  }
-  
-  ########################################################################################################################################################################
-  # identify the all leaf cell 
-  # identify the nearest leaf cells 
-  # connect leaf cells based on overlapping between kNN 
-  ########################################################################################################################################################################
-  # add some statistics below: 
-  cluster_inner_edges <- rep(0, n_cluster)
-  cluster_mat <- matrix(nrow = n_cluster, ncol = n_cluster)
-  cnt_i <- 1
-  pb4 <- utils::txtProgressBar(min = 0, max = length(unique(cell_membership)), style = 3, file = stderr())
-  for(i in unique(cell_membership)) {
-    cluster_inner_edges[i] <- igraph::ecount(igraph::subgraph(g, cell_names[which(cell_membership == i)])) # most are zero
-    cnt_j <- 1
-    for(j in setdiff(unique(cell_membership), i)) {
-      cell_i <- cell_names[which(cell_membership %in% c(i))]
-      cell_j <- cell_names[which(cell_membership %in% c(j))]
-      
-      all_ij <- igraph::ecount(igraph::subgraph(g, c(cell_i, cell_j))) # edges in all cells from two landmark groups
-      only_i <- igraph::ecount(igraph::subgraph(g, cell_i)) # edges from the first landmark groups
-      only_j <- igraph::ecount(igraph::subgraph(g, cell_j)) # edges from the second landmark groups
-      cluster_mat[i, j] <- (all_ij - only_i - only_j) / all_ij # edges overlapping between landmark groups
-      if(cluster_mat[i, j] > 0 & cluster_mat_exist[i, j] == 0) {
-        tmp <- as.matrix(dist(data[c(cell_i, cell_j), ]))
-        dist_mat <- tmp[1:length(cell_i), (length(cell_i) + 1):(length(cell_j) + length(cell_i))] # connection between cells from one group and that to another group 
-        ind_vec <- which(dist_mat == min(dist_mat), arr.ind = T)
-        pc_g %>% igraph::add_edges(c(c(cell_i, cell_j)[ind_vec[1]], c(cell_i, cell_j)[ind_vec[2]]))
-      }
-      cnt_j <- cnt_j + 1
-    }
-    cnt_i <- cnt_i + 1
-    utils::setTxtProgressBar(pb = pb4, value = pb4$getVal() + 1)
-  }
-  
-  ########################################################################################################################################################################
-  # downstream pseudotime and branch analysis 
-  ########################################################################################################################################################################
-  # identify branch
-  
-  # mst_branch_nodes <- V(minSpanningTree(cds))[which(degree(minSpanningTree(cds)) > 2)]$name
-  # cds@auxOrderingData[[cds@dim_reduce_type]]$cluster_mat_exist <- cluster_mat_exist
-  # cds@auxOrderingData[[cds@dim_reduce_type]]$cluster_mat <- cluster_mat
-  cluster_mat[is.na(cluster_mat)] <- 0
-  cluster_g <- igraph::graph_from_adjacency_matrix(cluster_mat, weighted = T, mode = 'undirected')
-  Qp <- -1
-  optim_res <- NULL
-  
-  louvain_iter <- 1
-  utils::pb5 <- txtProgressBar(max = length(louvain_iter), style = 3, file = stderr(), min = 0)
-  for (iter in 1:louvain_iter) {
-    Q <- igraph::cluster_louvain(cluster_g)
-    
-    if (is.null(optim_res)) {
-      Qp <- max(Q$modularity)
-      optim_res <- Q
-    }
-    else {
-      Qt <- max(Q$modularity)
-      if (Qt > Qp) {
-        optim_res <- Q
-        Qp <- Qt
-      }
-    }
-    utils::setTxtProgressBar(pb = pb5, value = pb5$getVal() + 1)
-  }
-  
-  if(verbose) {
-    message('clusters in the cluster graph is ', length(unique(igraph::membership(optim_res))))
-  }
-  # cds@auxOrderingData[[cds@dim_reduce_type]]$cluster_graph <- list(g = cluster_g, optim_res = optim_res)
-  if(length(igraph::E(cluster_g)) > 0) {
-    coord <- igraph::layout_components(cluster_g) 
-    coord <- as.data.frame(coord)
-    colnames(coord) <- c('x', 'y')
-    row.names(coord) <- 1:nrow(coord)
-    coord$Cluster <- 1:nrow(coord)
-    coord$louvain_cluster <- as.character(igraph::membership(optim_res))
-    
-    edge <- get.data.frame(cluster_g)
-    edge <- as.data.frame(edge)
-    colnames(edge) <- c('start', 'end', 'weight')
-    edge_links <- cbind(coord[edge$start, 1:2], coord[edge$end, 1:2])
-    edge_links <- as.data.frame(edge_links)
-    colnames(edge_links) <- c('x_start', 'x_end', 'y_start', 'y_end')
-    edge_links$weight <- edge[, 3]
-  } else {
-    coord <- NULL
-    edge_links <- NULL
-  }
-  
-  list(cluster_mat_exist = cluster_mat_exist, cluster_mat = cluster_mat, cluster_g = cluster_g, cluster_optim_res = optim_res, cluster_coord = coord, edge_links = edge_links)
-  
 }
